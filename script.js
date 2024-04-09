c43c05c7fdd1c550938ca671056a6dd390238859
Monjur203 <113359134+Monjur203@users.noreply.github.com>
2024-04-09 10:54:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
+
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnlogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+registerLink.addEventListener('click', ()=> {
+	wrapper.classList.add('active');
+});
+loginLink.addEventListener('click', ()=> {
+	wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+	wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+	wrapper.classList.remove('active-popup');
+});
